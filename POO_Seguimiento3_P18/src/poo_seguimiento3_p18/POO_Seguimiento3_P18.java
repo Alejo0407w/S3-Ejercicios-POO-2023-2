4bfe5bf2ed568cd5d3c191f4e37ba96f0d67517b
Alejandro Urrea Olarte <143052686+Alejo0407w@users.noreply.github.com>
2023-09-18 13:06:33 -0500

    Add files via upload

---
+++ POO_Seguimiento3_P18/src/poo_seguimiento3_p18/POO_Seguimiento3_P18.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo_seguimiento3_p18;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class POO_Seguimiento3_P18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario_P18 form=new Formulario_P18();
+        form.setVisible(true);
+    }
+    
+}
